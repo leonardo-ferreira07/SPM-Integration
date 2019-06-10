9eb684483bd449f76ab593a5b25079170d238b5a
Leonardo Kaminski Ferreira <leonardo@mindvalley.com>
2019-06-10 19:44:03 -0300

    Added project with SPM usage

---
+++ SPM-Integration/SPM-Integration/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SPM-Integration
+//
+//  Created by Leonardo Vinicius Kaminski Ferreira on 10/06/19.
+//  Copyright © 2019 Leonardo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
